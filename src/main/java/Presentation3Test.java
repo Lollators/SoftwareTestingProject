8595c081ae9158d2668688852c88cfe34854dc40
luca <gm.lollators@gmail.com>
2020-04-01 19:54:08 -0400

    Added Integration Testing

--- src/main/java/Presentation3Test.java
+++ src/main/java/Presentation3Test.java
@@ -1,26 +1,16 @@
+import java.io.FileNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.io.File;
-import java.io.IOException;
 import java.sql.SQLException;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class Presentation3Test {
-    @DisplayName("First Name")
-    @Test
-    void controllerOpenTest() {
-
-        assertDoesNotThrow (testFile.createNewFile());
-        assertThrows(SQLException.class, () -> mockAddressBookController.open(testFile));
-
-
-    }
 
     @Mock
     File testFile = new File("testFile.txt");
@@ -33,4 +23,32 @@ public class Presentation3Test {
     @Mock
     Person mockPerson = new Person("firstname", "lastname", "address",
             "city", "state", "zip", "phone");
+
+    @DisplayName("Test Save File")
+    @Test
+    void testSaveFile(){
+        FileSystem fs = new FileSystem();
+        assertDoesNotThrow(() -> fs.saveFile(mockAddressBook,testFile));
+    }
+
+    @DisplayName("Test Read File")
+    @Test
+    void testReadFile(){
+        FileSystem fs = new FileSystem();
+        assertDoesNotThrow(() -> fs.readFile(mockAddressBook,testFile));
+    }
+
+    @DisplayName("Address Book Integration Test - getFirstName")
+    @Test
+    public void addressBookIntegrationTest() {
+        AddressBook addressBook = mock(AddressBook.class);
+
+        //stub
+        when(addressBook.get(0)).thenReturn(mockPerson);
+
+        AddressBookController adr = new AddressBookController(addressBook);
+        assertEquals("firstname", adr.get(0).getFirstName());
+    }
+
+
 }
